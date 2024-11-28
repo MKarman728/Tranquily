59e1f744535ff93685d76fe6857d9ebf7e883a22
Mark Karman <mkarman08@gmail.com>
2024-11-28 09:50:02 -0800

    adding nested layout

---
+++ tutorial/app/tours/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+function error({ error }: { error: Error }) {
+  return <div>There was an error...</div>;
+}
+
+export default error;
---
+++ tutorial/app/tours/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoadingTours() {
+  return (
+    <div>
+      <h1>Loading Tours...</h1>
+    </div>
+  );
+}
+
+export default LoadingTours;
