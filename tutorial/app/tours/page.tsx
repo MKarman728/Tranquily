c0c6f2d59caf6c1310252aab8f801528bfa2af0c
Mark Karman <mkarman08@gmail.com>
2024-11-28 19:45:51 -0800

    created a dynamic route in NextJS

--- tutorial/app/tours/page.tsx
+++ tutorial/app/tours/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const url = "https://www.course-api.com/react-tours-project";
@@ -18,7 +19,13 @@ async function ToursPage() {
     <div>
       <h1 className="text-3xl mb-4">Tours</h1>
       {data.map((tour) => (
-        <h2 id={tour.id}>{tour.name}</h2>
+        <Link
+          id={tour.id}
+          href={`tours/${tour.id}`}
+          className="hover:text-blue-500"
+        >
+          <h1>{tour.name}</h1>
+        </Link>
       ))}
     </div>
   );
