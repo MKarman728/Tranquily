64a166edff96cc218a2147ea12b43cc05dfdef49
Mark Karman <mkarman08@gmail.com>
2024-11-29 14:57:03 -0800

    add local image

--- tutorial/app/tours/[id]/page.tsx
+++ tutorial/app/tours/[id]/page.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Beach from "@/images/alexander-akimenko-tWWdVrUSeik-unsplash.jpg";
+import Image from "next/image";
 // adding Beach image
 function IdPage({ params }: { params: { id: string } }) {
   return (
     <div>
       <h1 className="text-4xl">ID: {params.id}</h1>
+      <section className="flex gap-x-4 mt-4">
+        {/* Local image */}
+        <div className="">
+          <Image
+            src={Beach}
+            alt="map"
+            priority
+            className="w-48 h-48 object-cover rounded"
+            width={192}
+            height={192}
+          />
+          <h2>Local Image</h2>
+        </div>
+        {/* Remote image */}
+        <div></div>
+      </section>
     </div>
   );
 }
--- tutorial/components/Navbar.tsx
+++ tutorial/components/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="flex gap-x-4 mx-auto max-w-3xl py-3 border-b-2">
       <Link href="/">Home</Link>
-      <Link href="/count">Counter</Link>
+      <Link href="/counter">Counter</Link>
       <Link href="/tours">Tours</Link>
       <Link href="/actions">Actions</Link>
     </nav>
