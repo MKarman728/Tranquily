985c5f6687f6057a325e042475f4fe6dfb56d408
Mark Karman <mkarman08@gmail.com>
2024-11-29 23:00:36 -0800

    add images to tour options

--- tutorial/app/tours/page.tsx
+++ tutorial/app/tours/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const url = "https://www.course-api.com/react-tours-project";
 
@@ -14,19 +15,30 @@ type Tour = {
 async function ToursPage() {
   const response = await fetch(url);
   const data: Tour[] = await response.json();
-  console.log(data);
   return (
     <div>
       <h1 className="text-3xl mb-4">Tours</h1>
-      {data.map((tour) => (
-        <Link
-          id={tour.id}
-          href={`tours/${tour.id}`}
-          className="hover:text-blue-500"
-        >
-          <h1>{tour.name}</h1>
-        </Link>
-      ))}
+      <div className="grid gap-8 md:grid-cols-2">
+        {data.map((tour) => (
+          <Link
+            id={tour.id}
+            href={`tours/${tour.id}`}
+            className="hover:text-blue-500"
+          >
+            <div className="relative h-48 mb-2">
+              <Image
+                src={tour.image}
+                alt={tour.name}
+                fill
+                sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw"
+                priority
+                className="object-cover rounded"
+              />
+            </div>
+            <h1>{tour.name}</h1>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
