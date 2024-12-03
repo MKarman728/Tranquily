9423919493fdcd7541cb42d42b199471cfb15554
Mark Karman <mkarman08@gmail.com>
2024-12-02 19:53:45 -0800

    completed userslist

--- tutorial/components/UsersList.tsx
+++ tutorial/components/UsersList.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { fetchUsers } from "@/app/utils/actions";
 
-function UsersList() {
-  return <h3 className="text-xl">Users List</h3>;
+async function UsersList() {
+  const users = await fetchUsers();
+  return (
+    <div className="mt-4">
+      {users.length ? (
+        <div>
+          {users.map((user) => (
+            <h4 key={user.id} className="capitalize text-lg">
+              {user.firstName} {user.lastName}
+            </h4>
+          ))}
+        </div>
+      ) : (
+        <p>No users found.</p>
+      )}
+    </div>
+  );
 }
 
 export default UsersList;
