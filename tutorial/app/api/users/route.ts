a119e9c48f1fae7ac03be2a764a6b357bbe03cb0
Mark Karman <mkarman08@gmail.com>
2024-12-04 12:35:21 -0800

    create middleware file

--- tutorial/app/api/users/route.ts
+++ tutorial/app/api/users/route.ts
@@ -6,5 +6,12 @@ export const GET = async (req: NextRequest) => {
   console.log(req.nextUrl.searchParams.get("id"));
 
   const users = await fetchUsers();
-  return NextResponse.redirect(new URL("/", req.url));
+  return Response.json({ users });
+};
+
+export const POST = async (req: Request) => {
+  const user = await req.json();
+  const newUser = { ...user, id: Date.now().toString() };
+  await saveUser(newUser);
+  return Response.json({ msg: "user created" });
 };
