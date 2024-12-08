0c903fc966c26de9ebae2636a0fa6b6c381dbb00
Mark Karman <mkarman08@gmail.com>
2024-12-07 21:23:09 -0800

    adding create page

--- Home_Away/app/profile/create/page.tsx
+++ Home_Away/app/profile/create/page.tsx
@@ -1,8 +1,11 @@
-import { Button } from "@/components/ui/button";
-
-const createProfileAction = async (formData: FormData) => {
+import FormContainer from "@/components/form/FormContainner";
+import FormInput from "@/components/form/FormInput";
+import SubmitButton from "@/components/form/Buttons";
+import { Sub } from "@radix-ui/react-dropdown-menu";
+const createProfileAction = async (prevState: any, formData: FormData) => {
   "use server";
   const firstName = formData.get("firstName") as string;
+  return { message: "Profile Created" };
 };
 
 function CreateProfilePage() {
@@ -10,8 +13,15 @@ function CreateProfilePage() {
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
       <div className="border p-8 rounded-md max-w-lg">
-        <form action={createProfileAction}>test</form>
+        <FormContainer action={createProfileAction}>
+          <div className="grid gap-4 mt-4">
+            <FormInput type="text" name="firstName" label="First Name" />
+            <FormInput type="text" name="lastName" label="Last Name" />
+            <FormInput type="text" name="username" label="Username" />
+          </div>
+        </FormContainer>
       </div>
+      <SubmitButton text="Create Profile" className="mt-8" />
     </section>
   );
 }
