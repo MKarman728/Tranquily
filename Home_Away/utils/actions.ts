55e65b577feba009e0d77fd052c03551c801455d
Mark Karman <mkarman08@gmail.com>
2024-12-08 20:35:29 -0800

    adding update

--- Home_Away/utils/actions.ts
+++ Home_Away/utils/actions.ts
@@ -1,11 +1,17 @@
 "use server";
 import { profileSchema } from "./schemas";
+import db from "./db";
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
+    const user = await currentUser();
+    console.log(user);
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
     console.log(validatedFields);
