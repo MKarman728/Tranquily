0c07280d464e1c0c06125148b8d790fd4e6f77ea
Mark Karman <mkarman08@gmail.com>
2024-12-22 22:29:10 -0800

    comment section completed

--- components/reviews/Comment.tsx
+++ components/reviews/Comment.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { Button } from "../ui/button";
 
-function Comment() {
-  return <div>Comment</div>;
+function Comment({ comment }: { comment: string }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded((prev) => {
+      const newValue = !prev;
+      return newValue;
+    });
+  };
+  const longComment = comment.length > 130;
+  const displayComment =
+    longComment > !isExpanded ? `${comment.slice(0, 130)}...` : comment;
+  return (
+    <div>
+      <p className="text-sm">{displayComment}</p>
+      {longComment && (
+        <Button
+          variant="link"
+          className="pl-0 text-muted-foreground"
+          onClick={toggleExpanded}
+        >
+          {isExpanded ? "Show more" : "Show less"}
+        </Button>
+      )}
+    </div>
+  );
 }
 
 export default Comment;
